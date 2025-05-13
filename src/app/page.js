5946dbc9cd1dd3bc4400470769301958d50c9d7d
Gustavo Ferreira Dos Santos <gustavofrsnt@gmail.com>
2025-05-13 14:30:27 -0300

    Add / page frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 import styles from "./page.module.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "500", "900"]
+})
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Hello World</h1>
+      <h1 className={`${poppins.className} ${styles.title}`}><span className={`${poppins.className} ${styles.title_span}`}>YT</span> downloader</h1>
+      <input className={`${poppins.className} ${styles.url}`} type="text"/>
+      <button className={`${poppins.className} ${styles.buscar}`}>Buscar</button>
     </div>
   );
 }
